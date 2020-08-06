1fa203c0c83f901a87ac05a882131019c8637051
Dmitri <56200546+Syritx@users.noreply.github.com>
2020-08-06 18:58:03 -0400

    created Vector3

---
+++ Vector3.cs
@@ -0,0 +1,16 @@
+using System;
+namespace center
+{
+    public class Vector3
+    {
+        public float x { get; }
+        public float y { get; }
+        public float z { get; }
+
+        public Vector3(float x, float y, float z) {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+    }
+}
